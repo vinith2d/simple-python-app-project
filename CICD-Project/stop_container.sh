889c863c2ef7dcc9bd738f3c5e6c66ccaf3fdcd7
Vinith <vinith2k4@gmail.com>
2025-10-06 12:14:53 +0530

    Update stop_container.sh

--- CICD-Project/stop_container.sh
+++ CICD-Project/stop_container.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Get all running container IDs
+container_ids=$(docker ps -q)
+
+if [ -n "$container_ids" ]; then
+  echo "Stopping and removing running containers..."
+  docker rm -f $container_ids
+else
+  echo "No running containers found."
+fi
