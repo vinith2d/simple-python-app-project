9676c8064386200ec371f03fb54d434e012ad8a8
Vinith <vinith2k4@gmail.com>
2025-10-06 12:15:48 +0530

    Update start_container.sh

--- CICD-Project/start_container.sh
+++ CICD-Project/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push vinith21/simple-python-flask-app:Python_App
+docker pull vinith21/simple-python-flask-app:python_app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vinith21/simple-python-flask-app
+docker run -d -p 5000:5000 --name flask_app vinith21/simple-python-flask-app:python_app
